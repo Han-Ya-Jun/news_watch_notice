3d27baf2e7c5794ae7a79fa9ae4738de81f6651d
hanyajun <1581532052@qq.com>
2019-08-08 09:58:46 +0800

    :bug: Fixing a bug.

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -55,65 +55,64 @@ func PushGithub(token string, publish time.Time, contentList string) error {
 	}
 	oldContentList := strings.Split(string(decodeBytes), "## gocn_news_set_2019")
 	content.Content = []byte(oldContentList[0] + "\n" + "## gocn_news_set_2019" + "\n" + "### gocn_news_" + publish.Format("2006-01-02") + "\n" + contentList + "\n" + oldContentList[1])
-	repos, _, err := client.Repositories.UpdateFile(ctx, "Han-Ya-Jun", "gocn_news_set", "README.md", content)
-	fmt.Println(repos.SHA)
+	_, _, err = client.Repositories.UpdateFile(ctx, "Han-Ya-Jun", "gocn_news_set", "README.md", content)
 	if err != nil {
 		println(err)
 		return err
 	}
-	//获取ref
-	ref, _, err := client.Git.GetRef(ctx, "Han-Ya-Jun", "gocn_news_set", "heads/master")
-	if err != nil && ref == nil {
-		println(err)
-		return err
-	}
-	//获取commit
-	com, _, err := client.Git.GetCommit(ctx, "Han-Ya-Jun", "gocn_news_set", *ref.Object.SHA)
-	if err != nil && com == nil {
-		println(err)
-		return err
-	}
-	blob := &github.Blob{
-		Content:  github.String("add gocn news"),
-		Encoding: github.String("utf-8"),
-	}
-	//生成blob
-	result, _, err := client.Git.CreateBlob(ctx, "Han-Ya-Jun", "gocn_news_set", blob)
-	if err != nil && result == nil {
-		println(err)
-		return err
-	}
-	//生成tree
-	te := github.TreeEntry{
-		SHA:  result.SHA,
-		Path: github.String("README.md"),
-		Mode: github.String("100644"),
-		Type: github.String("blob"),
-	}
-	t := []github.TreeEntry{te}
-	tResult, _, err := client.Git.CreateTree(ctx, "Han-Ya-Jun", "gocn_news_set", *com.Tree.SHA, t)
-	if err != nil && tResult == nil {
-		println(err)
-		return err
-	}
-	//生成commit
-	comm := &github.Commit{
-		Message: github.String(c),
-		Parents: []github.Commit{
-			github.Commit{
-				SHA: ref.Object.SHA,
-			},
-		},
-		Tree: &github.Tree{
-			SHA: tResult.SHA,
-		},
-	}
-	cResult, _, err := client.Git.CreateCommit(ctx, "Han-Ya-Jun", "gocn_news_set", comm)
-	if err != nil && cResult == nil {
-		println(err)
-		return err
-	}
-	println(fmt.Sprintf("%v", cResult))
+	////获取ref
+	//ref, _, err := client.Git.GetRef(ctx, "Han-Ya-Jun", "gocn_news_set", "heads/master")
+	//if err != nil && ref == nil {
+	//	println(err)
+	//	return err
+	//}
+	////获取commit
+	//com, _, err := client.Git.GetCommit(ctx, "Han-Ya-Jun", "gocn_news_set", *ref.Object.SHA)
+	//if err != nil && com == nil {
+	//	println(err)
+	//	return err
+	//}
+	//blob := &github.Blob{
+	//	Content:  github.String("add gocn news"),
+	//	Encoding: github.String("utf-8"),
+	//}
+	////生成blob
+	//result, _, err := client.Git.CreateBlob(ctx, "Han-Ya-Jun", "gocn_news_set", blob)
+	//if err != nil && result == nil {
+	//	println(err)
+	//	return err
+	//}
+	////生成tree
+	//te := github.TreeEntry{
+	//	SHA:  result.SHA,
+	//	Path: github.String("README.md"),
+	//	Mode: github.String("100644"),
+	//	Type: github.String("blob"),
+	//}
+	//t := []github.TreeEntry{te}
+	//tResult, _, err := client.Git.CreateTree(ctx, "Han-Ya-Jun", "gocn_news_set", *com.Tree.SHA, t)
+	//if err != nil && tResult == nil {
+	//	println(err)
+	//	return err
+	//}
+	////生成commit
+	//comm := &github.Commit{
+	//	Message: github.String(c),
+	//	Parents: []github.Commit{
+	//		github.Commit{
+	//			SHA: ref.Object.SHA,
+	//		},
+	//	},
+	//	Tree: &github.Tree{
+	//		SHA: tResult.SHA,
+	//	},
+	//}
+	//cResult, _, err := client.Git.CreateCommit(ctx, "Han-Ya-Jun", "gocn_news_set", comm)
+	//if err != nil && cResult == nil {
+	//	println(err)
+	//	return err
+	//}
+	//println(fmt.Sprintf("%v", cResult))
 	return nil
 
 }
