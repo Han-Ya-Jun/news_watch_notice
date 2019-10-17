c79fb512063d5aef05b9f8d30222630a9dc84c52
hanyajun <1581532052@qq.com>
2019-10-17 18:20:12 +0800

    :bug: Fixing a bug.

--- pkg/reptile/reptile.go
+++ pkg/reptile/reptile.go
@@ -56,13 +56,17 @@ func GetNewsContent(publishTime time.Time) (e error, content []string) {
 			title2, _ := regexp.Compile(title)
 			matched := title2.FindAllStringSubmatchIndex(e.Text, -1)
 			indexList := rm.FindAllStringSubmatchIndex(e.Text, -1)
-			for i, v := range matched[:len(matched)-1] {
-				if v[0] <= indexList[len(indexList)-1][1] {
+			for i, v := range matched {
+				if v[0] <= indexList[len(indexList)-1][1] && i<len(matched)-1 {
 					content := e.Text[v[0]:matched[i+1][0]]
-					if strings.Contains(content, "编辑:") {
-						content = strings.Split(content, "编辑:")[0]
-					}
 					contentList = append(contentList, content+"\n")
+				}
+				if i==len(matched)-1{
+					content := e.Text[v[0]:]
+					if strings.Contains(content, "编辑:") {
+						index:=strings.Index(content,"编辑:")
+						content= e.Text[v[0]:index]
+					}
 				}
 			}
 		}
