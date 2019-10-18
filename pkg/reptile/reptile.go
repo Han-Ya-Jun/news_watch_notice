6566ecf74fd7b92f4698243a1968afe66e12dee1
hanyajun <1581532052@qq.com>
2019-10-18 12:48:58 +0800

    :bug: Fixing a bug.

--- pkg/reptile/reptile.go
+++ pkg/reptile/reptile.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gocolly/colly"
-	"news_watch_notice/utils"
 	"regexp"
 	"strings"
 	"time"
@@ -56,27 +55,20 @@ func GetNewsContent(publishTime time.Time) (e error, content []string) {
 			rm, _ := regexp.Compile(reg)
 			title2, _ := regexp.Compile(title)
 			matched := title2.FindAllStringSubmatchIndex(e.Text, -1)
-			fmt.Println(e.Text)
-			fmt.Println("******************.")
-			fmt.Println(matched)
-			fmt.Println("****************url")
 			indexList := rm.FindAllStringSubmatchIndex(e.Text, -1)
 			fmt.Println(indexList)
+			index:=strings.Index(e.Text,"编辑:")
 			for i, v := range matched {
-				if v[0] <= indexList[len(indexList)-1][1] && i < utils.GetMin(len(indexList),len(matched))-1 {
+				if v[0] <=index && i < len(matched)-1 {
 					content := e.Text[v[0]:matched[i+1][0]]
-					contentList = append(contentList, content+"\n")
-				}else{
-					content := e.Text[v[0]:]
 					if strings.Contains(content, "编辑:") {
 						index := strings.Index(content, "编辑:")
 						content = content[:index]
 						contentList=append(contentList, content+"\n")
 						break
 					}
+					contentList = append(contentList, content+"\n")
 				}
-
-
 			}
 		}
 	})
