abac11006c56b81d705c657e2091df996b222314
hanyajun <1581532052@qq.com>
2019-10-21 11:10:58 +0800

    :bug: Fixing a bug.

--- pkg/reptile/reptile.go
+++ pkg/reptile/reptile.go
@@ -50,7 +50,7 @@ func GetNewsContent(publishTime time.Time) (e error, content []string) {
 	var contentList []string
 	b.OnHTML("div.mod-body > div", func(e *colly.HTMLElement) {
 		if e.Text != "" {
-			reg := "^http://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$"
+			reg := "[a-zA-z]+://[^\\s]*"
 			title := "[1-5]\\."
 			rm, _ := regexp.Compile(reg)
 			title2, _ := regexp.Compile(title)
@@ -59,18 +59,20 @@ func GetNewsContent(publishTime time.Time) (e error, content []string) {
 			matched := title2.FindAllStringSubmatchIndex(e.Text, -1)
 			fmt.Println(matched)
 			indexList := rm.FindAllStringSubmatchIndex(e.Text, -1)
+			urls := rm.FindAllStringSubmatchIndex(e.Text, -1)
+			fmt.Printf("urls:%v\n", urls)
 			fmt.Println("***********************************************************")
 			fmt.Println(indexList)
 			fmt.Println("***********************************************************")
 			fmt.Println(e.Text[indexList[0][0]:indexList[0][1]])
-			index:=strings.Index(e.Text,"编辑:")
+			index := strings.Index(e.Text, "编辑:")
 			for i, v := range matched {
-				if v[0] <=index && i < len(matched)-1 {
+				if v[0] <= index && i < len(matched)-1 {
 					content := e.Text[v[0]:matched[i+1][0]]
 					if strings.Contains(content, "编辑:") {
 						index := strings.Index(content, "编辑:")
 						content = content[:index]
-						contentList=append(contentList, content+"\n")
+						contentList = append(contentList, content+"\n")
 						break
 					}
 					contentList = append(contentList, content+"\n")
